390b2cbd7ecb7258460581668704131f86c38b62
Kanui <marley.kanui@gmail.com>
2020-04-14 18:52:24 -0500

    Roma - Commit 11 - Updated quote_controller.rb index, create, show, and destroy actions

--- app/controllers/api/v1/quotes_controller.rb
+++ app/controllers/api/v1/quotes_controller.rb
@@ -1,13 +1,38 @@
 class Api::V1::QuotesController < ApplicationController
   def index
+    quote = Quote.all.order(created_at: :desc)
+    render json: quote
   end
 
   def create
+    quote = Quote.create!(quote_params)
+    if quote
+      render json: quote
+    else 
+      render json: quote.errors
+    end
   end
 
   def show
+    if quote
+      render json: quote
+    else 
+      render json: quote.errors
+    end
   end
 
   def destroy
+    quote&.destroy
+    render json: { message: 'Quote Deleted' }
+  end
+
+  private 
+
+  def quote_params
+    params.permit(:first_name, :last_name, :email, :number, :project_info)
+  end
+
+  def quote
+    @quote ||= Quote.find(params[:id])
   end
 end
