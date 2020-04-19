546bed7009052fb35aacc46f58ef2433504f9383
Kanui <marley.kanui@gmail.com>
2020-04-19 13:29:38 -0500

    Roma - Commit 23 - Media Query Tests - 1.5

--- app/javascript/components/Services.jsx
+++ app/javascript/components/Services.jsx
@@ -4,31 +4,6 @@ const Serv = () => {
   return (
     <div clasName="row text-center services">
       <div className="col-12">
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
         <h2 className="display-4">Section Two</h2>
       </div>
       <hr />
