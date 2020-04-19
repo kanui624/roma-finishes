5d3d1a6f4b68a769260da38777ff31049684c112
Kanui <marley.kanui@gmail.com>
2020-04-18 21:32:29 -0500

    Roma - Commit 17 - Added Navbar class Exchange on Scroll

--- app/javascript/packs/scroll.js
+++ app/javascript/packs/scroll.js
@@ -9,6 +9,19 @@ $(function () {
 });
 
 $(function () {
+  var nav_collapse = $(".navbar-dark");
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 200) {
+      nav_collapse.removeClass("navbar-dark").addClass("navbar-light");
+    } else {
+      nav_collapse.removeClass("navbar-light").addClass("navbar-dark");
+    }
+  });
+});
+
+$(function () {
   $(window).on("scroll", function () {
     if ($(window).scrollTop() > 10) {
       $(".navbar .navbar-brand img").attr(
