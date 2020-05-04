db33a3ffe9b82cfe9c9b97775cddd81444eaded0
Kanui <marley.kanui@gmail.com>
2020-05-04 13:54:29 -0500

    Roma - Global Commit 90 - FAQ Multiple UI Test - Pre Footer

--- app/javascript/components/Faq.jsx
+++ app/javascript/components/Faq.jsx
@@ -4,397 +4,60 @@ import spin from "../images/logo/spin.png";
 class Faq extends React.Component {
   render() {
     return (
-      <div className="faq-body">
-        <div className="faq-1 q-a mb-4">
-          <div className="around-faq-btn ">
-            <button className="faq">
-              <div className="row">
-                <div className="col-md-8 text-center">
-                  01 - Ex fugiat eiusmod magna fugiat velit ullamco
-                  reprehenderit cupidatat
-                </div>
-                <div className="col-md-3"></div>
-                <div className="col-md-1 text-center">
-                  <img
-                    id="faq-img-button"
-                    className="faq-img-button"
-                    src={require("../images/logo/spin.png")}
-                    alt="roma btn"
-                  />
-                </div>
+      <div>
+        <div className="faq-1 card mb-3">
+          <button className="faq" data-toggle="collapse" data-target="#coll1">
+            <div className="row">
+              <div className="faq-title col-md-8 text-center">
+                01 - Ex fugiat eiusmod magna fugiat velit ullamco reprehenderit
+                cupidatat
               </div>
-            </button>
-            <div className="container faq-answer">
-              <p>
-                Elit adipisicing sunt pariatur aute sint. Commodo esse tempor
-                officia ipsum cupidatat pariatur dolor. Exercitation aute ut
-                cupidatat aliqua sint laborum fugiat sint nulla non dolor.
-              </p>
+              <div className="col-md-3" />
+              <div className="col-md-1 text-center">
+                <img
+                  id="faq-img-button"
+                  className="faq-img-button"
+                  src={require("../images/logo/spin.png")}
+                  alt="roma btn"
+                />
+              </div>
             </div>
+          </button>
+          <div className="container faq-answer collapse text-center" id="coll1">
+            <hr />
+            <p>
+              Elit adipisicing sunt pariatur aute sint. Commodo esse tempor
+              officia ipsum cupidatat pariatur dolor. Exercitation aute ut
+              cupidatat aliqua sint laborum fugiat sint nulla non dolor.
+            </p>
           </div>
         </div>
 
-        <div className="faq-1 q-a mb-4">
-          <div className="around-faq-btn ">
-            <button className="faq">
-              <div className="row">
-                <div className="col-md-8 text-center">
-                  01 - Ex fugiat eiusmod magna fugiat velit ullamco
-                  reprehenderit cupidatat
-                </div>
-                <div className="col-md-3"></div>
-                <div className="col-md-1 text-center">
-                  <img
-                    className="faq-img-button"
-                    src={require("../images/logo/spin.png")}
-                    alt="roma btn"
-                  />
-                </div>
+        <div className="faq-1 card">
+          <button className="faq" data-toggle="collapse" data-target="#coll2">
+            <div className="row">
+              <div className="faq-title col-md-8 text-center">
+                01 - Ex fugiat eiusmod magna fugiat velit ullamco reprehenderit
+                cupidatat
               </div>
-            </button>
-            <div className="container faq-answer">
-              <p>
-                Elit adipisicing sunt pariatur aute sint. Commodo esse tempor
-                officia ipsum cupidatat pariatur dolor. Exercitation aute ut
-                cupidatat aliqua sint laborum fugiat sint nulla non dolor.
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="faq-1 q-a mb-4">
-          <div className="around-faq-btn ">
-            <button className="faq">
-              <div className="row">
-                <div className="col-md-8 text-center">
-                  01 - Ex fugiat eiusmod magna fugiat velit ullamco
-                  reprehenderit cupidatat
-                </div>
-                <div className="col-md-3"></div>
-                <div className="col-md-1 text-center">
-                  <img
-                    className="faq-img-button"
-                    src={require("../images/logo/spin.png")}
-                    alt="roma btn"
-                  />
-                </div>
+              <div className="col-md-3" />
+              <div className="col-md-1 text-center">
+                <img
+                  id="faq-img-button"
+                  className="faq-img-button"
+                  src={require("../images/logo/spin.png")}
+                  alt="roma btn"
+                />
               </div>
-            </button>
-            <div className="container faq-answer">
-              <p>
-                Elit adipisicing sunt pariatur aute sint. Commodo esse tempor
-                officia ipsum cupidatat pariatur dolor. Exercitation aute ut
-                cupidatat aliqua sint laborum fugiat sint nulla non dolor.
-              </p>
             </div>
-          </div>
-        </div>
-
-        <div className="faq-1 q-a mb-4">
-          <div className="around-faq-btn ">
-            <button className="faq">
-              <div className="row">
-                <div className="col-md-8 text-center">
-                  01 - Ex fugiat eiusmod magna fugiat velit ullamco
-                  reprehenderit cupidatat
-                </div>
-                <div className="col-md-3"></div>
-                <div className="col-md-1 text-center">
-                  <img
-                    className="faq-img-button"
-                    src={require("../images/logo/spin.png")}
-                    alt="roma btn"
-                  />
-                </div>
-              </div>
-            </button>
-            <div className="container faq-answer">
-              <p>
-                Elit adipisicing sunt pariatur aute sint. Commodo esse tempor
-                officia ipsum cupidatat pariatur dolor. Exercitation aute ut
-                cupidatat aliqua sint laborum fugiat sint nulla non dolor.
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="faq-1 q-a mb-4">
-          <div className="around-faq-btn ">
-            <button className="faq">
-              <div className="row">
-                <div className="col-md-8 text-center">
-                  01 - Ex fugiat eiusmod magna fugiat velit ullamco
-                  reprehenderit cupidatat
-                </div>
-                <div className="col-md-3"></div>
-                <div className="col-md-1 text-center">
-                  <img
-                    className="faq-img-button"
-                    src={require("../images/logo/spin.png")}
-                    alt="roma btn"
-                  />
-                </div>
-              </div>
-            </button>
-            <div className="container faq-answer">
-              <p>
-                Elit adipisicing sunt pariatur aute sint. Commodo esse tempor
-                officia ipsum cupidatat pariatur dolor. Exercitation aute ut
-                cupidatat aliqua sint laborum fugiat sint nulla non dolor.
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="faq-1 q-a mb-4">
-          <div className="around-faq-btn ">
-            <button className="faq">
-              <div className="row">
-                <div className="col-md-8 text-center">
-                  01 - Ex fugiat eiusmod magna fugiat velit ullamco
-                  reprehenderit cupidatat
-                </div>
-                <div className="col-md-3"></div>
-                <div className="col-md-1 text-center">
-                  <img
-                    className="faq-img-button"
-                    src={require("../images/logo/spin.png")}
-                    alt="roma btn"
-                  />
-                </div>
-              </div>
-            </button>
-            <div className="container faq-answer">
-              <p>
-                Elit adipisicing sunt pariatur aute sint. Commodo esse tempor
-                officia ipsum cupidatat pariatur dolor. Exercitation aute ut
-                cupidatat aliqua sint laborum fugiat sint nulla non dolor.
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="faq-1 q-a mb-4">
-          <div className="around-faq-btn ">
-            <button className="faq">
-              <div className="row">
-                <div className="col-md-8 text-center">
-                  01 - Ex fugiat eiusmod magna fugiat velit ullamco
-                  reprehenderit cupidatat
-                </div>
-                <div className="col-md-3"></div>
-                <div className="col-md-1 text-center">
-                  <img
-                    className="faq-img-button"
-                    src={require("../images/logo/spin.png")}
-                    alt="roma btn"
-                  />
-                </div>
-              </div>
-            </button>
-            <div className="container faq-answer">
-              <p>
-                Elit adipisicing sunt pariatur aute sint. Commodo esse tempor
-                officia ipsum cupidatat pariatur dolor. Exercitation aute ut
-                cupidatat aliqua sint laborum fugiat sint nulla non dolor.
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="faq-1 q-a mb-4">
-          <div className="around-faq-btn ">
-            <button className="faq">
-              <div className="row">
-                <div className="col-md-8 text-center">
-                  01 - Ex fugiat eiusmod magna fugiat velit ullamco
-                  reprehenderit cupidatat
-                </div>
-                <div className="col-md-3"></div>
-                <div className="col-md-1 text-center">
-                  <img
-                    className="faq-img-button"
-                    src={require("../images/logo/spin.png")}
-                    alt="roma btn"
-                  />
-                </div>
-              </div>
-            </button>
-            <div className="container faq-answer">
-              <p>
-                Elit adipisicing sunt pariatur aute sint. Commodo esse tempor
-                officia ipsum cupidatat pariatur dolor. Exercitation aute ut
-                cupidatat aliqua sint laborum fugiat sint nulla non dolor.
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="faq-1 q-a mb-4">
-          <div className="around-faq-btn ">
-            <button className="faq">
-              <div className="row">
-                <div className="col-md-8 text-center">
-                  01 - Ex fugiat eiusmod magna fugiat velit ullamco
-                  reprehenderit cupidatat
-                </div>
-                <div className="col-md-3"></div>
-                <div className="col-md-1 text-center">
-                  <img
-                    className="faq-img-button"
-                    src={require("../images/logo/spin.png")}
-                    alt="roma btn"
-                  />
-                </div>
-              </div>
-            </button>
-            <div className="container faq-answer">
-              <p>
-                Elit adipisicing sunt pariatur aute sint. Commodo esse tempor
-                officia ipsum cupidatat pariatur dolor. Exercitation aute ut
-                cupidatat aliqua sint laborum fugiat sint nulla non dolor.
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="faq-1 q-a mb-4">
-          <div className="around-faq-btn ">
-            <button className="faq">
-              <div className="row">
-                <div className="col-md-8 text-center">
-                  01 - Ex fugiat eiusmod magna fugiat velit ullamco
-                  reprehenderit cupidatat
-                </div>
-                <div className="col-md-3"></div>
-                <div className="col-md-1 text-center">
-                  <img
-                    className="faq-img-button"
-                    src={require("../images/logo/spin.png")}
-                    alt="roma btn"
-                  />
-                </div>
-              </div>
-            </button>
-            <div className="container faq-answer">
-              <p>
-                Elit adipisicing sunt pariatur aute sint. Commodo esse tempor
-                officia ipsum cupidatat pariatur dolor. Exercitation aute ut
-                cupidatat aliqua sint laborum fugiat sint nulla non dolor.
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="faq-1 q-a mb-4">
-          <div className="around-faq-btn ">
-            <button className="faq">
-              <div className="row">
-                <div className="col-md-8 text-center">
-                  01 - Ex fugiat eiusmod magna fugiat velit ullamco
-                  reprehenderit cupidatat
-                </div>
-                <div className="col-md-3"></div>
-                <div className="col-md-1 text-center">
-                  <img
-                    className="faq-img-button"
-                    src={require("../images/logo/spin.png")}
-                    alt="roma btn"
-                  />
-                </div>
-              </div>
-            </button>
-            <div className="container faq-answer">
-              <p>
-                Elit adipisicing sunt pariatur aute sint. Commodo esse tempor
-                officia ipsum cupidatat pariatur dolor. Exercitation aute ut
-                cupidatat aliqua sint laborum fugiat sint nulla non dolor.
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="faq-1 q-a mb-4">
-          <div className="around-faq-btn ">
-            <button className="faq">
-              <div className="row">
-                <div className="col-md-8 text-center">
-                  01 - Ex fugiat eiusmod magna fugiat velit ullamco
-                  reprehenderit cupidatat
-                </div>
-                <div className="col-md-3"></div>
-                <div className="col-md-1 text-center">
-                  <img
-                    className="faq-img-button"
-                    src={require("../images/logo/spin.png")}
-                    alt="roma btn"
-                  />
-                </div>
-              </div>
-            </button>
-            <div className="container faq-answer">
-              <p>
-                Elit adipisicing sunt pariatur aute sint. Commodo esse tempor
-                officia ipsum cupidatat pariatur dolor. Exercitation aute ut
-                cupidatat aliqua sint laborum fugiat sint nulla non dolor.
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="faq-1 q-a mb-4">
-          <div className="around-faq-btn ">
-            <button className="faq">
-              <div className="row">
-                <div className="col-md-8 text-center">
-                  01 - Ex fugiat eiusmod magna fugiat velit ullamco
-                  reprehenderit cupidatat
-                </div>
-                <div className="col-md-3"></div>
-                <div className="col-md-1 text-center">
-                  <img
-                    className="faq-img-button"
-                    src={require("../images/logo/spin.png")}
-                    alt="roma btn"
-                  />
-                </div>
-              </div>
-            </button>
-            <div className="container faq-answer">
-              <p>
-                Elit adipisicing sunt pariatur aute sint. Commodo esse tempor
-                officia ipsum cupidatat pariatur dolor. Exercitation aute ut
-                cupidatat aliqua sint laborum fugiat sint nulla non dolor.
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="faq-1 q-a mb-4">
-          <div className="around-faq-btn ">
-            <button className="faq">
-              <div className="row">
-                <div className="col-md-8 text-center">
-                  01 - Ex fugiat eiusmod magna fugiat velit ullamco
-                  reprehenderit cupidatat
-                </div>
-                <div className="col-md-3"></div>
-                <div className="col-md-1 text-center">
-                  <img
-                    className="faq-img-button"
-                    src={require("../images/logo/spin.png")}
-                    alt="roma btn"
-                  />
-                </div>
-              </div>
-            </button>
-            <div className="container faq-answer">
-              <p>
-                Elit adipisicing sunt pariatur aute sint. Commodo esse tempor
-                officia ipsum cupidatat pariatur dolor. Exercitation aute ut
-                cupidatat aliqua sint laborum fugiat sint nulla non dolor.
-              </p>
-            </div>
+          </button>
+          <div className="container faq-answer collapse text-center" id="coll2">
+            <hr />
+            <p>
+              Elit adipisicing sunt pariatur aute sint. Commodo esse tempor
+              officia ipsum cupidatat pariatur dolor. Exercitation aute ut
+              cupidatat aliqua sint laborum fugiat sint nulla non dolor.
+            </p>
           </div>
         </div>
       </div>
--- app/javascript/packs/onFaq.js
+++ app/javascript/packs/onFaq.js
@@ -1,29 +1,8 @@
-// // IMG ON CLICK
-
-// // DROP DOWN ON CLICK
-// // $(document).ready(function () {
-// //   var faqs = $(document.getElementsByClassName("faq"));
-// //   for (var i = 0; i < faqs.length; i++) {
-// //     faqs[i].onclick = function () {
-// //       var answer = $(document.getElementsByClassName("faq-answer"));
-// //       if (answer.style.maxHeight) {
-// //         answer.style.maxHeight = null;
-// //       } else {
-// //         answer.style.maxHeight = answer.scrollHeight + "px";
-// //       }
-// //     };
-// //   }
-// // });
-
-// $(document).ready(function () {
-//   const faqs = $(".faq");
-//   for (var i = 0; i < faqs.length; i++) {
-//     faqs[i].click = function () {
-//       const answer = $(".faq-answer");
-//       if (answer.style.)
+// $(document).ready(() => {
+//   let faqs = $(".faq-box-shadow");
+//   for (let i = 0; i < faqs.length; i++) {
+//     if ($(i).click) {
+//       i.removeClass("faq-box-shadow").addClass("no-box-shadow");
 //     }
 //   }
-//     $(".faq").click(function () {
-//       answer.toggle();
-//     });
 // });
